a08bcb022bce2ab406b2ae8197ee1784ec2abdaf
jabenjaben <shaldira@mail.ru>
2019-05-22 21:21:13 +0300

    симулятор жизни - 3 старый ucfDecoder

--- src/x/gui/Map.java
+++ src/x/gui/Map.java
@@ -1,60 +1,70 @@
 package x.gui;
 
-import x.ucf.UcfCoder;
-
-import javax.swing.*;
+        import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
-import java.util.Random;
 
-public class Map extends JTable {
+        public class Map extends JTable {
 
-    Random random = new Random();
+        //	private Random random = new Random();
 
-    private int mapSize = 129;
-    private int cellSize = 5;
+        	private final int mapSize = 129;
+	private final int cellSize = 5;
 
-    public Map(){
-        setModel(new DefaultTableModel(this.mapSize, this.mapSize));
-        setRowHeight(this.cellSize);
-        for(int i = 0; i < this.mapSize; i++){
-            getColumnModel().getColumn(i).setMinWidth(this.cellSize);
-            getColumnModel().getColumn(i).setMaxWidth(this.cellSize);
+        	public Map() {
+        		setModel(new DefaultTableModel(this.mapSize, this.mapSize));
+        		setRowHeight(this.cellSize);
+        		for (int i = 0; i < this.mapSize; i++) {
+            			this.getColumnModel().getColumn(i).setMinWidth(this.cellSize);
+            			this.getColumnModel().getColumn(i).setMaxWidth(this.cellSize);
+            		}
+        		for (int x = 0; x < this.mapSize; x++) {
+            			for (int y = 0; y < this.mapSize; y++) {
+                				setValueAt(0x0000, y, x);
+                			}
+            		}
+        //		generateLandscape();
+                		setVisible(true);
+        	}
+        //	private final void generateLandscape() {
+        //
+        //		setValueAt(UcfCoder.LANDSCAPE_MOUNTAIN_LOW, 0, 0);
+        //		setValueAt(UcfCoder.LANDSCAPE_MOUNTAIN_LOW, 0, this.mapSize - 1);
+        //		setValueAt(UcfCoder.LANDSCAPE_MOUNTAIN_LOW, this.mapSize - 1, 0);
+        //		setValueAt(UcfCoder.LANDSCAPE_MOUNTAIN_LOW, this.mapSize - 1, this.mapSize - 1);
+        //
+        //		float landscapeShift = 15.0f;
+        //
+        //		for (int bigStep = this.mapSize - 1; bigStep >= 2; bigStep /= 2, landscapeShift /= 2.0f) {
+        //			int smallStep = bigStep / 2;
+        //			// Diamond step - build diamonds
+        //			for (int x = smallStep; x < this.mapSize; x += bigStep) {
+        //				for (int y = smallStep; y < this.mapSize; y += bigStep) {
+        //					int topLeftValue = (int)getValueAt(y - smallStep, x - smallStep);
+        //					int topRightValue = (int)getValueAt(y - smallStep, x + smallStep);
+        //					int bottomLeftValue = (int)getValueAt(y + smallStep, x - smallStep);
+        //					int bottomRightValue = (int)getValueAt(y + smallStep, x + smallStep);
+        //					float avg =  (topLeftValue + topRightValue + bottomLeftValue + bottomRightValue) / 4;
+        //					int centerValue = getValueInRange(avg + this.random.nextInt(3) * landscapeShift - landscapeShift, 1, 15);
+        //					setValueAt(centerValue, y, x);
+        //				}
+        //			}
+        //			// Square step - build squares
+        //			for (int x = 0; x < this.mapSize; x += smallStep) {
+        //				for (int y = (x + smallStep) % bigStep; y < this.mapSize; y += bigStep) {
+        //					int topValue = (int)getValueAt((y - smallStep + this.mapSize - 1) % (this.mapSize - 1), x);
+        //					int leftValue = (int)getValueAt(y, (x - smallStep + this.mapSize - 1) % (this.mapSize - 1));
+        //					int rightValue = (int)getValueAt(y, (x + smallStep) % (this.mapSize - 1));
+        //					int bottomValue = (int)getValueAt((y + smallStep) % (this.mapSize - 1), x);
+        //					float avg =  (topValue + leftValue + rightValue + bottomValue) / 4;
+        //					int centerValue = getValueInRange(avg + this.random.nextInt(3) * landscapeShift - landscapeShift, 1, 15);
+        //					setValueAt(centerValue, y, x);
+        //				}
+        //			}
+        //		}
+        //	}
+
+        //	private int getValueInRange(float value, int min, int max) {
+        //		return (int)Math.max(Math.min(value, max), min);
+        //	}
+
         }
-
-        for (int i = 0; i < this.mapSize; i++){
-            for (int j = 0; j < this.mapSize; j++){
-                setValueAt(0x0000, j, i);
-            }
-        }
-        generateLandscape();
-    }
-
-    private final void generateLandscape(){
-        setValueAt(UcfCoder.LANDSCAPE_MOUNTAIN_LOW, 0, 0);
-        setValueAt(UcfCoder.LANDSCAPE_MOUNTAIN_LOW, 0, 0 + this.mapSize - 1);
-        setValueAt(UcfCoder.LANDSCAPE_MOUNTAIN_LOW, 0 + this.mapSize - 1, 0);
-        setValueAt(UcfCoder.LANDSCAPE_MOUNTAIN_LOW, 0 + this.mapSize - 1, 0 + this.mapSize - 1);
-
-        float landscapeShif = 15.0f;
-
-        for(int bigStep = this.mapSize - 1; bigStep >= 2; bigStep /= 2, landscapeShif /=2.0){
-            int smallStep = bigStep / 2;
-            //Diamond step
-            for (int x = smallStep; x < this.mapSize; x += bigStep){
-                for (int y = smallStep; y < this.mapSize; y++ ){
-                    int topLeftValue = (int)(getValueAt(y - smallStep, x - smallStep));
-                    int topRightValue = (int)(getValueAt(y - smallStep, x + smallStep));
-                    int bottomLeftValue = (int)(getValueAt(y + smallStep, x - smallStep));
-                    int bottomRightValue = (int)(getValueAt(y + smallStep, x + smallStep));
-                    float average = (topLeftValue + topRightValue + bottomLeftValue + bottomRightValue)/4;
-                    float centralValue = average + random.nextInt(3) * landscapeShif - landscapeShif;
-                    setValueAt(centralValue, y, x);
-                }
-            }
-        }
-    }
-
-    private static  final  int getValueRange(int value, int min, int max){
-        return Math.max(Math.min(value, max), min);
-    }
-}
